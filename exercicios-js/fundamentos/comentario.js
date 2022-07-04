cda52e05c7bc9b478b462c14275a0e6c52cd1326
Kamila Harkatin <108098371+Harkatin@users.noreply.github.com>
2022-07-04 16:51:06 -0300

    exercicios ate o 12

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,13 @@
+//COMENTARIOS DE UMA ÚNICA LINHA
+console.log ('linha1')
+
+/*Comentário de várias linhas
+por exemplo
+aqui */
+console.log ('linha2')
+
+/*
+*comentário de várias linhas
+*tipo isso
+*/
+console.log ('linha3')
